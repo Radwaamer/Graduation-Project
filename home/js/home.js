e73a9699ddbbab5add12f805ada516d900d63835
radwa amer <rororado44@gmail.com>
2023-02-23 09:50:23 -0800

    add elastic search and update home page

--- home/js/home.js
+++ home/js/home.js
@@ -1,16 +1,61 @@
 // create landing section
 function landing(){
-    fetch("../json/pwnedItems.json")
-    .then(res=>res.json())
-    .then(data=>{
-        for(let i=0;i<4;i++){
-            document.querySelector(".landing .row").innerHTML+=`
-            <div class="col-6 col-md-3 mb-4">
-            <span class="text-white d-block fw-bold fs-4">${data["pwnedItems"][i]["number"].toLocaleString()}</span>
-            <i class="fs-3 my-2 fa-solid fa-file"></i>
-            <p>${data["pwnedItems"][i]["item"]}</p>
-            </div>`
-        };
-    });
+    const pwned ={
+        "pwnedItems":[
+            {
+                "id":1,
+                "number":632,
+                "icon":"<i class='fs-3 my-3 fa-solid fa-window-restore'></i>",
+                "item":"pwned websites"
+            },
+            {
+                "id":2,
+                "number":11991408974,
+                "icon":"<i class='fs-3 my-3 fa-solid fa-file-invoice'></i>",
+                "item":"pwned accounts"
+            },
+            {
+                "id":3,
+                "number":115522,
+                "icon":"<i class='fs-3 my-3 fa-solid fa-paste'></i>",
+                "item":"pastes"
+            },
+            {
+                "id":4,
+                "number":223531362,
+                "icon":"<i class='fs-3 my-3 fa-solid fa-clipboard'></i>",
+                "item":"paste accounts"
+            }
+        ]
+    }
+    for(let i=0;i<4;i++){
+        document.querySelector(".landing .row").innerHTML+=`
+        <div class="col-6 col-md-3 mb-4">
+        <span class="text-white d-block fw-bold fs-4">${pwned["pwnedItems"][i]["number"].toLocaleString()}</span>
+        ${pwned["pwnedItems"][i]["icon"]}
+        <p>${pwned["pwnedItems"][i]["item"]}</p>
+        </div>`
+    };
 };
 landing();
+
+// increase number of statics
+function increase(){
+    let i=11936681242;
+    setInterval(function(){
+        document.querySelectorAll(".landing .row span")[1].innerHTML=i.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        i++;
+    },2500);
+    let x=223169978;
+    setInterval(function(){
+        document.querySelectorAll(".landing .row span")[3].innerHTML=x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        x++;
+    },1500)
+    if(i=11936681242+999){
+        i=11936681242;
+    };
+    if(x=223169978+999){
+        x=223169978;
+    };
+};
+increase();
