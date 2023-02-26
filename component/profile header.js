94d5ba12976732e3e43ecc44157d1f786781ae24
radwa amer <rororado44@gmail.com>
2023-02-26 09:31:43 -0800

    add user profile

--- component/profile header.js
+++ component/profile header.js
@@ -25,14 +25,14 @@ function getInfo(){
             document.querySelector("header .info").innerHTML=
                 `<img class="rounded-circle position-absolute start-50 translate-middle-x" src="${login.img}" alt="${login.username}">
                 <p class="user fw-bold black-color mt-3 mb-4">${login.username}</p>
-                <button class="btn border-0 main-btn w-100 text-white py-2 d-flex align-items-center justify-content-center gap-2">
+                <a href="../user profile/user.html" class="btn border-0 main-btn w-100 text-white py-2 d-flex align-items-center justify-content-center gap-2">
                     <i class="fs-5 fa-regular fa-circle-user"></i>
                     <p>View Profile</p>
-                </button>
-                <button class="btn border-0 main-btn w-100 text-white py-2 mt-3 d-flex align-items-center justify-content-center gap-2">
+                </a>
+                <a href="../home/home.html" class="btn border-0 main-btn w-100 text-white py-2 mt-3 d-flex align-items-center justify-content-center gap-2">
                     <i class="fs-5 fa-solid fa-arrow-right-from-bracket"></i>
                     <p>Log Out</p>
-                </button>`
+                </a>`
         }
         else if(this.status==404 && this.readyState==4){
             document.querySelectorAll("header nav .profile").forEach(profile=>{
