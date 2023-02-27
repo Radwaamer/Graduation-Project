6c3c9c26186616f700a2cf6eb1a9efd95ef8d70e
radwa amer <rororado44@gmail.com>
2023-02-27 14:22:37 -0800

    add multi links option

--- public/frontend/home/js/elastic search.js
+++ public/frontend/home/js/elastic search.js
@@ -33,18 +33,19 @@ function doSearch() {
             }
         ];
 
+        let breach=false;
             const arr=(num,key)=>{
-                links[num][key].map(link=>{
+                for(let i=0;i<links[num][key].length;i++){
                     var body = {
                         size: 50,
                     };
                     var query = {
                         bool: {},
                     };
-                    query.bool.must=link.queryBool
+                    query.bool.must=links[num][key][i].queryBool
                     body.query = query;
                     var xmlHttp = new XMLHttpRequest();
-                    xmlHttp.open("POST", link.host, false);
+                    xmlHttp.open("POST", links[num][key][i].host, false);
                     xmlHttp.setRequestHeader(
                         "Content-Type",
                         "application/json;charset=UTF-8"
@@ -52,44 +53,13 @@ function doSearch() {
                     xmlHttp.send(JSON.stringify(body));
                     var response = xmlHttp.responseText;
 
-                    if (JSON.parse(response)["hits"]["total"]["value"] == 0) {
-                        document.getElementById("my-template").innerHTML = `
-                            <swal-title>
-                                Good news
-                            </swal-title>
-                            <swal-html>
-                                <div class="black-color">
-                                    <h4 class="suc">No pwnage found!</h4>
-                                    <p>No breached accounts and no pastes</p>
-                                </div>
-                            </swal-html>
-                            <swal-icon type="success" color="var(--main-color)"></swal-icon>
-                            <swal-button color="var(--main-color)" type="cancel">
-                                Great!
-                            </swal-button>
-                            <swal-footer><a href="../faq/faq.html#avoid">How to avoid breaches?</a></swal-footer>`;
-                    } else if (JSON.parse(response)["hits"]["total"]["value"] == 1) {
-                        document.getElementById("my-template").innerHTML = `
-                            <swal-title>
-                                Oops...
-                            </swal-title>
-                            <swal-html>
-                                <div class="black-color">
-                                    <h4 class="faild">Pwned!</h4>
-                                    <p>Pwned in data breaches and found pastes</p>
-                                </div>
-                            </swal-html>
-                            <swal-icon type="error" color="red"></swal-icon>
-                            <swal-button color="var(--main-color)" type="cancel">
-                                Ok
-                            </swal-button>
-                            <swal-footer><a href="../faq/faq.html#breached">I found that my email was breached, now what?</a></swal-footer>`;
+                    if(JSON.parse(response)["hits"]["total"]["value"] == 0){
+                        breach=false;
+                    }else{
+                        breach=true;
+                        break;
                     }
-                    Swal.fire({
-                        template: "#my-template",
-                    });
-
-                });
+                }
             }
 
 
@@ -99,6 +69,43 @@ function doSearch() {
                 }else{
                     arr(1,"numbers");
                 }
+
+                if (!breach) {
+                    document.getElementById("my-template").innerHTML = `
+                        <swal-title>
+                            Good news
+                        </swal-title>
+                        <swal-html>
+                            <div class="black-color">
+                                <h4 class="suc">No pwnage found!</h4>
+                                <p>No breached accounts and no pastes</p>
+                            </div>
+                        </swal-html>
+                        <swal-icon type="success" color="var(--main-color)"></swal-icon>
+                        <swal-button color="var(--main-color)" type="cancel">
+                            Great!
+                        </swal-button>
+                        <swal-footer><a href="../faq/faq.html#avoid">How to avoid breaches?</a></swal-footer>`;
+                } else if (breach) {
+                    document.getElementById("my-template").innerHTML = `
+                        <swal-title>
+                            Oops...
+                        </swal-title>
+                        <swal-html>
+                            <div class="black-color">
+                                <h4 class="faild">Pwned!</h4>
+                                <p>Pwned in data breaches and found pastes</p>
+                            </div>
+                        </swal-html>
+                        <swal-icon type="error" color="red"></swal-icon>
+                        <swal-button color="var(--main-color)" type="cancel">
+                            Ok
+                        </swal-button>
+                        <swal-footer><a href="../faq/faq.html#breached">I found that my email was breached, now what?</a></swal-footer>`;
+                }
+                Swal.fire({
+                    template: "#my-template",
+                });
 
 
     });
