d3f8c9ad95ee84f595e662bb12f48182e2467306
radwa amer <rororado44@gmail.com>
2023-03-12 10:14:48 -0700

    remove larvel pages and add pwa settings

--- resources/views/components/application-logo.blade.php
+++
@@ -1,1 +0,0 @@
-<img src="{{asset('logo/logo.png')}}" alt="logo" width="100px">
--- resources/views/searchlabs.blade.php
+++
@@ -1,8 +0,0 @@
-<h3>search results</h3>
-
-    <form action="{{url('searchlabs')}}" method="get">
-
-        <input type="text" value="{{$keyword}}" name="keyword" id="">
-        <br>
-        <button type="submit">search</button>
-        {{$keyword}}
